c1e4403d59679476f86fd10203e0bb618faebd86
podlv8mc <88781072+podlv8mc@users.noreply.github.com>
2022-09-23 15:27:18 +0300

    version003

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-
-
-
-
-const Item = ({item, author, setModalVisible, setModalData}) => (
-  <TouchableOpacity onPress={() => {setModalVisible(true);setModalData(item)} } >
-    <View style={styles.item}>
+/*
+<View style={styles.item}>
       <Text style={styles.title}>{item.title}</Text>
       <Text style={styles.description}>{item.description}</Text>
+    </View>
+*/
+
+const Item = ({item, author, setModalVisible, setModalData}) => (
+  <TouchableOpacity
+    onPress={() => {
+      setModalVisible(true);
+      setModalData(item);
+    }}>
+    <View style={styles.info}>
+      <View style={styles.infoBlok}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.description}>{item.description}</Text>
+      </View>
     </View>
   </TouchableOpacity>
 );
 
 export default Item;
 
+
+
+
+
+const styles = StyleSheet.create({
+  info: {
+    flex: 1,
+    backgroundColor: 'rgba(245, 245, 255, 0.85)',
+    borderRadius: 20,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    padding: 20,
+  },
+  infoBlok: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  title: {
+    flex: 1.6,
+    fontFamily: 'Times New Roman',
+    fontWeight: '700',
+    fontSize: 16,
+    lineHeight: 19.2,
+  },
+  description: {
+    fontFamily: 'Times New Roman',
+    fontWeight: '400',
+    fontSize: 14,
+    marginTop: 20,
+  },
+});
+
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -32,6 +74,6 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 18,
-    
   },
 });
+*/
